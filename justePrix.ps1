122c4e117a17b45fe2e9c151494ec107cf600895
PA EL FAMOSO <ENORMOUS@PENIS.lol>
2018-02-28 10:22:36 +0100

    snapshot 2 - class creer

--- justePrix.ps1
+++ justePrix.ps1
@@ -8,20 +8,33 @@ comparer le prix de l'objet avec le prix donné par l'util
 #creer tableau d'objet
 $Objets = ("RER B","Guitare","Chaise","Horloge","Batterie")
 
+#Creer une classe d'objet
+class ObjetJP {
+    [string]$Name
+    [int]$Price
+    }
+$ObjetJP = New-Object ObjetJP
+
 #Choose un objet en random
 $ChooseObject = Get-Random($Objets)
+    #je renseigne le nom de l'objet
+    $ObjetJP.Name = $ChooseObject
 
 #lui donner un prix
 switch($ChooseObject){
-    "RER B"{ Get-Random -Minimum
+    "RER B"{ $PriceObject = Get-Random -Minimum 15000 -Maximum 150000
         }
-    "Guitare"{
+    "Guitare"{ $PriceObject = Get-Random -Minimum 100 -Maximum 5000
         }
-    "Chaise"{
+    "Chaise"{ $PriceObject = Get-Random -Minimum 10 -Maximum 100
+        } 
+    "Horloge"{ $PriceObject = Get-Random -Minimum 500 -Maximum 2000
         }
-    "Horloge"{
-        }
-    "batterie"{
+    "batterie"{ $PriceObject = Get-Random -Minimum 500 -Maximum 5000
         }
     }
-    
+    #Je renseigne le prix de l'objet
+    $ObjetJP.Price = $PriceObject
+
+#DEBBUGING LINE
+$ObjetJP

efad92a6f07c71acfc7bbc5c67f9d9a2b2420c4a
PA EL FAMOSO <ENORMOUS@PENIS.lol>
2018-02-28 14:04:12 +0100

    mise en place d'une function

---
+++ FuncjustePrix.ps1
@@ -0,0 +1,65 @@
+﻿Function FuncJustePrix{
+    
+    Do{   
+        #Poser la question à l'utilisateur
+        do{
+            $AnswerUser = Read-Host "
+            #------------------------------------------------------------------#
+            #     Alors $($UserJP.Name) ! Quel est le prix de cet objet ?      #
+            #     (Son prix est compris entre $MiniPrice € et $MaxiPrice €)    #
+            #------------------------------------------------------------------#"
+            #DEBBUGUNG LINE#
+            Write-Host $ObjetJP.Price
+            #Check si il à rentré un chiffre
+
+              if ($AnswerUser -notmatch '^\d+$'){
+                  Write-Host "
+                  #------------------------------------------------------------------#
+                  #           Tu n'as pas rentré un chiffre, recomence               #
+                  #------------------------------------------------------------------#"
+                 }
+              else{
+                 $AnswerUser = [long]$AnswerUser
+                 }
+        }while ($AnswerUser -notmatch '^\d+$')
+
+        if ($AnswerUser -lt $ObjetJP.Price){
+            Write-Host "
+            #------------------------------------------------------------------#
+            #              C'est plus grand, tu perd une vie                   #
+            #------------------------------------------------------------------#"
+            $UserJP.Life--
+            $UserJP.Life
+        }
+        elseif ($AnswerUser -gt $ObjetJP.Price){
+            Write-Host "
+            #------------------------------------------------------------------#
+            #             C'est plus petit, tu perd une vie                    #
+            #------------------------------------------------------------------#"
+            $UserJP.Life--
+            $UserJP.Life
+        }
+        elseif ($AnswerUser -eq $ObjetJP.Price){
+            Read-Host "
+            #------------------------------------------------------------------#
+            #                      C'est ça ! Gagné !                          #
+            #           Appuie sur Entré pour ferme le programme               #
+            #------------------------------------------------------------------#"
+            Exit 0
+        }
+        else {
+            Write-Host "
+            #------------------------------------------------------------------#
+            #          Tu n'as pas rentré de chiffre, recomence                #
+            #------------------------------------------------------------------#"
+        }
+    }While ($UserJP.Life -gt 0)
+
+    Read-Host "
+    #------------------------------------------------------------------#
+    #                           Tu as perdu !                          #
+    #              Appuie sur Entrée pour fermer le jeu                #
+    #------------------------------------------------------------------#"
+    Exit 0
+
+}
--- justePrix.ps1
+++ justePrix.ps1
@@ -6,35 +6,156 @@ comparer le prix de l'objet avec le prix donné par l'util
 #>
 
 #creer tableau d'objet
-$Objets = ("RER B","Guitare","Chaise","Horloge","Batterie")
+$Objets = ("un RER B","une Guitare","une Chaise","une Horloge","une Batterie")
 
 #Creer une classe d'objet
 class ObjetJP {
     [string]$Name
-    [int]$Price
+    [long]$Price
+    [string]$ASCIIArt
     }
 $ObjetJP = New-Object ObjetJP
 
+#Créer une classe utilisateur
+class UserJP {
+    [String]$Name
+    [int]$Life = 5
+    }
+$UserJP = New-Object UserJP
+
+#créer function de jeu
+. .\FuncjustePrix.ps1
 #Choose un objet en random
 $ChooseObject = Get-Random($Objets)
     #je renseigne le nom de l'objet
     $ObjetJP.Name = $ChooseObject
 
-#lui donner un prix
+#lui donner un prix et un ASCII
 switch($ChooseObject){
-    "RER B"{ $PriceObject = Get-Random -Minimum 15000 -Maximum 150000
-        }
-    "Guitare"{ $PriceObject = Get-Random -Minimum 100 -Maximum 5000
-        }
-    "Chaise"{ $PriceObject = Get-Random -Minimum 10 -Maximum 100
-        } 
-    "Horloge"{ $PriceObject = Get-Random -Minimum 500 -Maximum 2000
-        }
-    "batterie"{ $PriceObject = Get-Random -Minimum 500 -Maximum 5000
-        }
+    "un RER B"{ 
+        [int]$MiniPrice = 15000
+        [int]$MaxiPrice = 150000
+        $PriceObject = Get-Random -Minimum $MiniPrice -Maximum $MaxiPrice
+        $ASCIIObject = "
+        
+                                                 /'   `\
+                                                Y.     .Y
+                                      _______    \`. .'/
+                       ,-------------'=======`"--""""-""""---.
+                 __,=+'-------------------------------------|p
+              .-/__|_]_]  :`"/:""""""""""""""""""""""""""""""|'
+           ,-'__________[];/_;_____________________RER B____|
+         ,`".../_|___________________________________________|
+        (_>        ,-------.                     ,-------.  |
+         `-._____.'(_)`='(_)\_7___7___7___7__7_.'(_)`='(_)\_/ tchou tchou_ nous seront en retard
+
+"}
+    "une Guitare"{ 
+        [int]$MiniPrice = 100
+        [int]$MaxiPrice = 5000
+        $PriceObject = Get-Random -Minimum $MiniPrice -Maximum $MaxiPrice
+        $ASCIIObject = "
+        
+     _
+    / 7
+   /_(
+   |_|
+   |_|
+   |_|
+   |_| /\
+ /\|=|/ /
+ \ |_| /
+  ) _  \
+ / |_|  \
+/  -=-o /
+\  /~\_/  -Row
+ \/
+
+        "}
+    "une Chaise"{ 
+        [int]$MiniPrice = 10
+        [int]$MaxiPrice = 100
+        $PriceObject = Get-Random -Minimum $MiniPrice -Maximum $MaxiPrice
+        $ASCIIObject = "
+        
+   i______i
+   I______I
+   I      I
+   I______I
+  /      /I
+ (______( I
+ I I    I I
+ I      I
+
+        "} 
+    "une Horloge"{ 
+        [int]$MiniPrice = 500
+        [int]$MaxiPrice = 2000
+        $PriceObject = Get-Random -Minimum $MiniPrice -Maximum $MaxiPrice
+        $ASCIIObject = "
+        
+                             O
+                            (_)
+                          _ )_( _
+                        /`_) H (_`\
+                      .' (  { }  ) '.
+                    _/ /` '-'='-' `\ \_
+                   [_.'   _,...,_   '._]
+                    |   .:`"`````":.   |
+                    |__//_________\\__|
+                     | .-----------. |
+                     | |  .-""`"-.  | |
+                     | | /    /  \ | |
+                     | ||-   <   -|| |
+                     | | \    \  / | |
+                     | |[`'-...-'`]| |
+                     | | ;-.___.-; | |
+                     | | |  |||  | | |
+                     | | |  |||  | | |
+                     | | |  |||  | | |
+                     | | |  |||  | | |
+                     | | |  |||  | | |
+                     | | | _|||_ | | |
+                     | | | >===< | | |
+                     | | | |___| | | |
+                     | | |  |||  | | |
+                     | | |  ;-;  | | |
+                     | | | (   ) | | |
+                     | | |  '-'  | | |
+                     | | '-------' | |
+                jgs _| '-----------' |_
+                   [= === === ==== == =]
+                   [__--__--___--__--__]
+                  /__-___-___-___-___-__\
+                 `"""""""""""""""""""""""`
+
+        "}
+    "une Batterie"{ 
+        [int]$MiniPrice = 500
+        [int]$MaxiPrice = 5000
+        $PriceObject = Get-Random -Minimum $MiniPrice -Maximum $MaxiPrice
+        $ASCIIObject = "
+        
+ .-.,      ,--. ,--.
+`/|~\     \__/T`--'     .
+x |`' __   ,-~^~-.___ ==I==
+  |  |--| /       \__}  |
+  |  |  |{   /~\   }    |
+ /|\ \__/ \  \_/  /|   /|\
+/ | \|  | /`~-_-~'X.\ //| \
+"}
     }
+
     #Je renseigne le prix de l'objet
     $ObjetJP.Price = $PriceObject
+    $ObjetJP.ASCIIArt = $ASCIIObject
 
-#DEBBUGING LINE
-$ObjetJP
+#Demander le nom de l'utilisateur
+$UserName = Read-Host "Quel est ton nom ?"
+$UserJP.name = $UserName
+
+#Affichage de l'enigme et lancement de la fucntion
+Write-host "L'objet d'aujourd'hui est $($ObjetJP.name)"
+write-host $ObjetJP.ASCIIArt
+FuncJustePrix
+
